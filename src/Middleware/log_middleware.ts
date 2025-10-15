2131e3ca93c8e301d32ab651dfb6dfe71c59ed4d
judithukachukwu <ukachukwujudith95@gmail.com>
2025-10-15 12:09:44 +0100

    fix: fix code error in missleware

--- src/Middleware/log_middleware.ts
+++ src/Middleware/log_middleware.ts
@@ -2,12 +2,12 @@ import {Request, Response, NextFunction } from "express";
 import { LogError } from "../common/types/error_types";
 
 
-export const logMiddleware = (req: Request, res: Response, next: NextFunction) {
+export const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
   // also verify that the user is authenticated
   if (!req.headers['X-logservice-timestamp'] || !req.headers['X-logservice-signature'])  {
     throw new LogError('credentials not found', 400)
   }
   const timestamp = req.headers['X-logservice-timestamp']
   const signature = req.headers['X-logservice-signature']
-  const 
+
 }
