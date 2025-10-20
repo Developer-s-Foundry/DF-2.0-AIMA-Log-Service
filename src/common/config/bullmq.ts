5d89b6e4a9a59f7f3d52eeb3f765f3c7812a4cd0
judithukachukwu <ukachukwujudith95@gmail.com>
2025-10-20 12:23:56 +0100

    add redis connection option to queue system

--- src/common/config/bullmq.ts
+++ src/common/config/bullmq.ts
@@ -1,4 +1,8 @@
 import { Queue } from "bullmq";
 import { APP_CONFIGS } from ".";
 
-export const logQueue = new Queue(APP_CONFIGS.QUEUE_NAME);
+export const logQueue = new Queue(APP_CONFIGS.QUEUE_NAME, {connection: {
+    host: APP_CONFIGS.REDIS_HOST , 
+    port: parseInt(APP_CONFIGS.REDIS_PORT), 
+    password: APP_CONFIGS.REDIS_PASSWORD
+}});
