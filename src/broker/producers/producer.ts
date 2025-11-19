70386f7be69056999e7bc383701f32a7432aac6f
judithukachukwu <ukachukwujudith95@gmail.com>
2025-11-19 05:59:54 +0100

    fix: remove setinterval function from publisher

--- src/broker/producers/producer.ts
+++ src/broker/producers/producer.ts
@@ -6,6 +6,9 @@ import { createChannel } from "../../common/config/rabbitmq";
 export const publishMsg = (msg: string) => {
     const exchangeName = 'logData';
     const routingKey = 'logs'
+    const queue2 = 'logData2'
+
+    console.log(`publish: ${msg} to producer`)
 
     createChannel()
     .then((channel) => {
@@ -23,14 +26,14 @@ export const publishMsg = (msg: string) => {
             }).catch(((error) => {
                 console.log(`unable to create a Queue${error}`);
             }));
-            channel.assertQueue('logData2', {durable: true})
-            channel.bindQueue(APP_CONFIGS.QUEUE_NAME_RMQ_2, exchangeName,routingKey)
-            channel.bindQueue('logData2', exchangeName,routingKey)
+            channel.assertQueue(queue2, {durable: true})
+            channel.bindQueue(APP_CONFIGS.QUEUE_NAME_RMQ_2, exchangeName,routingKey);
+            
+            channel.bindQueue(queue2, exchangeName,routingKey)
             .then(() => {
                 console.log(`bind to queue successfully`);
-                setInterval(() => channel.publish(
-                exchangeName, routingKey, Buffer.from(msg)), 1000)
-                console.log(`Sent to ${exchangeName}:`, msg);
+                channel.publish(
+                exchangeName, routingKey, Buffer.from(msg))
             }).catch(((error) => {
                 console.log(`unable to bind to a Queue${error}`);
             }));  
